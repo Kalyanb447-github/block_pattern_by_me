404de1e3920288f1f6466daf3c84e99f4945fcf2
Kalyan Biswas <kalyanb447@gmail.com>
2019-11-26 23:30:17 +0530

    commit

---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,122 @@
+class ItemModel {
+  int _page;
+  int _total_results;
+  int _total_pages;
+  List<_Result> _results = [];
+
+  ItemModel.fromJson(Map<String, dynamic> parsedJson) {
+    print(parsedJson['results'].length);
+    _page = parsedJson['page'];
+    _total_results = parsedJson['total_results'];
+    _total_pages = parsedJson['total_pages'];
+    List<_Result> temp = [];
+    for (int i = 0; i < parsedJson['results'].length; i++) {
+      _Result result = _Result(parsedJson['results'][i]);
+      temp.add(result);
+    }
+    _results = temp;
+  }
+
+  List<_Result> get results => _results;
+
+  int get total_pages => _total_pages;
+
+  int get total_results => _total_results;
+
+  int get page => _page;
+}
+
+class _Result {
+  int _vote_count;
+  int _id;
+  bool _video;
+  var _vote_average;
+  String _title;
+  double _popularity;
+  String _poster_path;
+  String _original_language;
+  String _original_title;
+  List<int> _genre_ids = [];
+  String _backdrop_path;
+  bool _adult;
+  String _overview;
+  String _release_date;
+
+  _Result(result) {
+    _vote_count = result['vote_count'];
+    _id = result['id'];
+    _video = result['video'];
+    _vote_average = result['vote_average'];
+    _title = result['title'];
+    _popularity = result['popularity'];
+    _poster_path = result['poster_path'];
+    _original_language = result['original_language'];
+    _original_title = result['original_title'];
+    for (int i = 0; i < result['genre_ids'].length; i++) {
+      _genre_ids.add(result['genre_ids'][i]);
+    }
+    _backdrop_path = result['backdrop_path'];
+    _adult = result['adult'];
+    _overview = result['overview'];
+    _release_date = result['release_date'];
+  }
+
+  String get release_date => _release_date;
+
+  String get overview => _overview;
+
+  bool get adult => _adult;
+
+  String get backdrop_path => _backdrop_path;
+
+  List<int> get genre_ids => _genre_ids;
+
+  String get original_title => _original_title;
+
+  String get original_language => _original_language;
+
+  String get poster_path => _poster_path;
+
+  double get popularity => _popularity;
+
+  String get title => _title;
+
+  double get vote_average => _vote_average;
+
+  bool get video => _video;
+
+  int get id => _id;
+
+  int get vote_count => _vote_count;
+}
+
+
+//json example
+/*{
+  "page": 1,
+  "total_results": 19772,
+  "total_pages": 989,
+  "results": [
+    {
+      "vote_count": 6503,
+      "id": 299536,
+      "video": false,
+      "vote_average": 8.3,
+      "title": "Avengers: Infinity War",
+      "popularity": 350.154,
+      "poster_path": "\/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+      "original_language": "en",
+      "original_title": "Avengers: Infinity War",
+      "genre_ids": [
+        12,
+        878,
+        14,
+        28
+      ],
+      "backdrop_path": "\/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+      "adult": false,
+      "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+      "release_date": "2018-04-25"
+    },
+
+    */
